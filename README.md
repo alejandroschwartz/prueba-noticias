a9957a9e0b051bc869f0fea8e753d6eb4c0302a9
Alejandro Schwartz <alejandro.schwartz@contaduria.marandu.com.ar>
2024-08-06 17:53:43 -0300

    Validando el formulario, agregandole tambien el select de los numeros por paises

---
+++ README.md
@@ -0,0 +1,41 @@
+# Prueba de Página de noticias
+
+Link al proyecto: [https://prueba-noticias-trece.netlify.app](https://prueba-noticias-trece.netlify.app/)
+
+![](https://res.cloudinary.com/dhdhpvhkg/image/upload/q_80/v1722973000/examen/imagen-app.webp)
+
+## Descripción de las tareas a desarrollar
+
+Clonar esta landing page con sus mismas funcionalidades, tales como:
+
+- Tabla de ganancias
+
+- Formulario con bandera del pais y que si se valide el teléfono con una libreria de validacion de número validos.
+
+- Popup de ganancia
+
+Dar feedback de como lo mejorarias. 
+
+## Desarrollo e instalacion del proyecto
+
+El proyecto lo realicé con HTML, CSS con la libreria de Bootstrap y con Javascript. Utilice el empaquetador de proyectos Webpack con una caonfiguracion rápida para poder desarrolla y realizar un deploy facilmente.
+
+### Para instalarlo, ejecutar:
+
+```js
+npm i
+
+npm start
+```
+
+Corre en el puerto [http://localhost:3000](http://localhost:3000)
+
+## Mejoras implementadas
+
+- Primero alinear los distintos contenidos, dejando un tamaño maximo para las pantallas mas grandes. Tambien mejore el nav para mobile.
+
+- Cambie el diseño de la tabla para que sea mas legible y facil de entender, con los títulos en negritas. Los circulos verdes no aportan, asi que lo cambie por el color en el monto (asumi que era positivo pero podria tener en rojo alguno en perdida).
+
+- Para el formulario, quite el background rojo del título, asi lo que más resalta seria el el mismo botón de registrar y lo delimite.
+
+
--- src/index.js
+++ src/index.js
@@ -2,29 +2,106 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import Toastify from 'toastify-js';
 import "toastify-js/src/toastify.css";
+import intlTelInput from 'intl-tel-input';
+import 'intl-tel-input/build/css/intlTelInput.css';
+import { parsePhoneNumber, isValidPhoneNumber } from 'libphonenumber-js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('El documento está listo!');
 
     // Ejemplo de uso de Toastify
-    Toastify({
-        text: "¡41 personas estan viendo esto!",
-        duration: 3000,
-        style: {
-            background: "#999",
-        },
-    }).showToast();
-
-    // Ejemplo de uso de Toastify despues de 5 segundos con duracion de 3000 milisegundos
     setTimeout(() => {
         Toastify({
-            text: "¡José López \n acaba de generar ₡2.940.283 \n de un depósito de ₡135.000!",
+            text: "¡41 personas están viendo esto!",
             duration: 3000,
             style: {
-                background: "#fff",
-                color: "#000",
+                background: "#777",
             },
         }).showToast();
-    }, 5000);
+    }, 2000);
 
+    // Ejemplo de Toast con css para cargar las imagenes
+    setTimeout(() => {
+        const toast = document.getElementById('toast-card');
+        toast.style.display = 'block';
+        toast.classList.remove('toast-hide');
+    
+        setTimeout(() => {
+            toast.classList.add('toast-hide');
+            setTimeout(() => {
+                toast.style.display = 'none';
+            }, 500);
+        }, 5000);
+    }, 8000);
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const input = document.querySelector("#phone");
+    const iti = intlTelInput(input, {
+        initialCountry: "auto",
+        separateDialCode: true,
+        geoIpLookup: (success, failure) => {
+            fetch("https://ipapi.co/json")
+                .then((res) => res.json())
+                .then((data) => success(data.country_code))
+                .catch(() => failure());
+        }
+    });
+
+    let countryData;
+    input.addEventListener('countrychange', () => {
+        countryData = iti.getSelectedCountryData();
+    });
+
+    const form = document.querySelector("form");
+    const resultado = document.createElement("div");
+    form.after(resultado);
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        // Obtengo los valores de los campos del formulario
+        const name = document.getElementById("name").value;
+        const surname = document.getElementById("surname").value;
+        const email = document.getElementById("email").value;
+        const phone = `+${countryData.dialCode} ${document.getElementById("phone").value}`;
+        const phoneParse = parsePhoneNumber(phone, countryData.country);
+        
+        // Verifico el número de teléfono ingresado
+        if (!isValidPhoneNumber(phoneParse.number, phoneParse.country)) {
+            resultado.innerHTML = `
+                <div class="container-fluid text-center alert alert-danger" role="alert">
+                    El número de teléfono no es válido.
+                </div>
+            `;
+            resultado.style.color = "red";
+            return;
+        }
+
+        // Verifico que los campos del formulario no estén vacíos
+        if (!name || !email || !surname || !phone) {
+            resultado.innerHTML = `
+                <div class="container-fluid text-center alert alert-danger" role="alert">
+                    Por favor, completa todos los campos.
+                </div>
+            `;
+            resultado.style.color = "red";
+            return;
+        }
+
+        // Muestro los datos ingresados en el formulario
+        resultado.innerHTML = `
+            <div class="container-fluid text-center alert alert-success d-flex flex-column" role="alert">
+                <h4>¡FELICIDADES!</h4>
+                <br/>
+                <p>
+                    Los datos ingresados son: <strong>Nombre:</strong> ${name}
+                    , <strong>Apellido:</strong> ${surname}
+                    , <strong>Email:</strong> ${email}
+                    , <strong>Teléfono:</strong> ${phone}
+                </p>
+            </div>
+        `;
+        resultado.style.color = "green";
+    });
 });
