b416d988371c8c4e046bd83bf3005021e9f709cc
Alejandro Schwartz <alejandro.schwartz@contaduria.marandu.com.ar>
2024-08-07 15:07:38 -0300

    Cambiando el renderizado de la tabla para que sea random cada 5 seg, tambien el boton de registrarse con pulso

--- README.md
+++ README.md
@@ -32,13 +32,15 @@ Corre en el puerto [http://localhost:3000](http://localhost:3000)
 
 ## Mejoras implementadas
 
-- Primero alinear los distintos contenidos, dejando un tamaño maximo para las pantallas mas grandes. Tambien mejore el nav para mobile.
+- Primero alinear los distintos contenidos, dejando un tamaño maximo para las pantallas más grandes. Tambien mejore el nav para mobile.
 
-- Cambie el diseño de la tabla para que sea mas legible y facil de entender, con los títulos en negritas. Los circulos verdes no aportan, asi que lo cambie por el color en el monto (asumi que era positivo pero podria tener en rojo alguno en perdida).
+- Para los Popup de ganancias,los genere de dos tipos, uno simple (librería toastify-js) que aparece a los 3 segundos y otro lo arme con css, bootstrap para mostrar la imagen y los montos que aparece a los 8 segundos.
 
-- Para el formulario, quite el background rojo del título, asi lo que más resalta seria el el mismo botón de registrar y lo delimite.
+- Cambie el diseño de la tabla para que sea mas legible y facil de entender, con los títulos en negritas. Los circulos verdes no aportan, asi que lo cambie por el color en el monto (asumí que era positivo pero podría tener en rojo o negro alguno en perdida o igual). La tabla se actualiza cada 5 segundos, tomando datos aleatorios de un array de personas.
 
-- Verfique las mejoras de lighthouse: Para tener el mejor rendimiento tanto para el usuario como para el posicionamiento SEO en Google
+- Para el formulario, quite el background rojo del título, asi lo que más resalta seria el el mismo botón de registrar y lo delimite. Genere el formulario con las validaciones, con el select  de las banderas para el teléfono (librería intl-tel-input) y la validación de los teléfonos (librería libphonenumber-js), mostrando los datos en pantalla al “registrarse ahora”. También el botón de "¡REGISTRARME AHORA!" con el efecto de pulso.
+
+- Verfique las mejoras de lighthouse: Para tener el mejor rendimiento tanto para el usuario como para el posicionamiento SEO en Google.
 
 ![](https://res.cloudinary.com/dhdhpvhkg/image/upload/c_scale,q_80,w_1080/v1722978636/examen/imagen-lighthouse.webp)
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,30 @@ import intlTelInput from 'intl-tel-input';
 import 'intl-tel-input/build/css/intlTelInput.css';
 import { parsePhoneNumber, isValidPhoneNumber } from 'libphonenumber-js';
 
+// Creo un array con datos de las personas para mostrar aleatoriamente en la tabla
+const users = [
+    { id: "1", nombre: "Josefa", ganancia: "770.930", ciudad: "Ciudad de México" },
+    { id: "2", nombre: "Ana", ganancia: "1.126.880", ciudad: "Buenos Aires" },
+    { id: "3", nombre: "Carmen", ganancia: "827.538", ciudad: "São Paulo" },
+    { id: "4", nombre: "Rosa", ganancia: "1.032.190", ciudad: "Río de Janeiro" },
+    { id: "5", nombre: "Laura", ganancia: "1.264.743", ciudad: "Bogotá" },
+    { id: "6", nombre: "Marta", ganancia: "1.168.444", ciudad: "Lima" },
+    { id: "7", nombre: "Flor", ganancia: "940.952", ciudad: "Santiago" },
+    { id: "8", nombre: "Andrea", ganancia: "289.139", ciudad: "Caracas" },
+    { id: "9", nombre: "Adriana", ganancia: "622.601", ciudad: "Quito" },
+    { id: "10", nombre: "Olga", ganancia: "389.628", ciudad: "La Paz" },
+    { id: "11", nombre: "Sara", ganancia: "1.285.890", ciudad: "Montevideo" },
+    { id: "12", nombre: "María", ganancia: "353.994", ciudad: "Asunción" },
+    { id: "13", nombre: "Luisa", ganancia: "888.908", ciudad: "San Salvador" },
+    { id: "14", nombre: "Sofía", ganancia: "1.294.696", ciudad: "Ciudad de Guatemala" },
+    { id: "15", nombre: "Elena", ganancia: "708.162", ciudad: "Tegucigalpa" },
+    { id: "16", nombre: "Rita", ganancia: "965.190", ciudad: "Managua" },
+    { id: "17", nombre: "Clara", ganancia: "650.200", ciudad: "San José" },
+    { id: "18", nombre: "Paula", ganancia: "231.428", ciudad: "Santo Domingo" },
+    { id: "19", nombre: "Ana", ganancia: "609.621", ciudad: "La Habana" },
+    { id: "20", nombre: "Carmen", ganancia: "1.235.265", ciudad: "Panamá" },
+];
+
 document.addEventListener('DOMContentLoaded', () => {
 
     // Ejemplo de uso de Toastify
@@ -19,23 +43,37 @@ document.addEventListener('DOMContentLoaded', () => {
         }).showToast();
     }, 2000);
 
-    // Ejemplo de Toast con css para cargar las imagenes
+    // Ejemplo de Toast con css para cargar las imagenes que comienza a los 8 segundos
     setTimeout(() => {
         const toast = document.getElementById('toast-card');
         toast.style.display = 'block';
         toast.classList.remove('toast-hide');
-    
-        setTimeout(() => {
+        // Desaparece a los 5 segundos despues de empezar
+        setTimeout(() => {  
             toast.classList.add('toast-hide');
+            // Efecto de 500milisegnuos para desaparecer
             setTimeout(() => {
                 toast.style.display = 'none';
             }, 500);
         }, 5000);
     }, 8000);
-});
 
-
-document.addEventListener("DOMContentLoaded", () => {
+    // Creo el cuerpo de la tabla con los datos de personas con solo 10 personas aleatorias
+    const tbody = document.querySelector('table tbody');
+    function updateTable() {
+        const randomUsers = users.sort(() => 0.5 - Math.random());
+        const randomSelection = randomUsers.slice(0, 10);
+        const filasHtml = randomSelection.map(dato => `
+            <tr>
+                <td>${dato.nombre}</td>
+                <td class="text-success">₡ ${dato.ganancia}</td>
+                <td>${dato.ciudad}</td>
+            </tr>
+        `).join('');
+        tbody.innerHTML = filasHtml;
+    }
+    updateTable(); // Es para la carga inicial de la tabla
+    setInterval(updateTable, 5000); // Actualizo la tabla cada 5 segundos
 
     // Calculo de la fecha actual 
     let fechaActual = new Date();
@@ -48,6 +86,7 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById('barraInferior').style.display = 'none';
     };
 
+    // Traigo el input de teléfono y lo inicializo la libreria intlTelInput con el codigo del pais del usuario
     const input = document.querySelector("#phone");
     const iti = intlTelInput(input, {
         initialCountry: "auto",
@@ -60,6 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    // inicio variable y guardo el pais seleccionado para concatenar a lo escrito y luego validarlo con la libreria libphonenumber-js
     let countryData;
     input.addEventListener('countrychange', () => {
         countryData = iti.getSelectedCountryData();
@@ -69,6 +109,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const resultado = document.createElement("div");
     form.after(resultado);
 
+    // envento de submit al clickear el boton "¡REGISTRARME AHORA!"
     form.addEventListener("submit", (event) => {
         event.preventDefault();
 
@@ -79,7 +120,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const phone = `+${countryData.dialCode} ${document.getElementById("phone").value}`;
         const phoneParse = parsePhoneNumber(phone, countryData.country);
         
-        // Verifico el número de teléfono ingresado
+        // Verifico el número de teléfono ingresado con la libreria libphonenumber-js
         if (!isValidPhoneNumber(phoneParse.number, phoneParse.country)) {
             resultado.innerHTML = `
                 <div class="container-fluid text-center alert alert-danger" role="alert">
